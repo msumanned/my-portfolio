8fbe7079b13c78aef576098c0825672751d8dfcb
muhammad usman <msumanned@gmail.com>
2023-09-26 16:34:06 +0500

    github actions added

--- app.js
+++ app.js
@@ -5,4 +5,22 @@ menuIcon.onclick = ()=>{
 
     menuList.classList.toggle("open");
 }
+let sections = document.querySelectorAll('section');
+let menu_lists = document.querySelectorAll('header ul li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            menu_lists.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header ul li a[href*= '+ id +']').classList.add('active');
+            });
+        }
+    });
+};
 
